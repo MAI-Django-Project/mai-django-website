3f16867cee9545a1374c82a21a03fd0241b0d3d9
Aleshka5 <fad80223@gmail.com>
2025-01-24 23:51:56 +0300

    latest update

--- marketapp/models.py
+++ marketapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from usersapp.models import Shopper
+
 # Create your models here.
 
 
@@ -8,26 +9,32 @@ class Clients(models.Model):
     f = models.CharField(max_length=50)
     i = models.CharField(max_length=50)
     o = models.CharField(max_length=50)
-    age = models.DateField(null=True,blank=True)
+    age = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length=12)
     pswrd = models.CharField(max_length=30)
+
     def __str__(self):
-        return (self.f+' '+self.i+' '+self.o)
+        return self.f + " " + self.i + " " + self.o
+
 
 class Managers(models.Model):
     zp = models.IntegerField()
     pswrd = models.CharField(max_length=30)
 
+
 class Top_managers(models.Model):
     zp = models.IntegerField()
     pswrd = models.CharField(max_length=30)
 
+
 class Reps(models.Model):
     top_manager_id = models.ForeignKey(Top_managers, on_delete=models.CASCADE)
+
 
 class Markets(models.Model):
     manager_id = models.ForeignKey(Managers, on_delete=models.CASCADE)
     id_rep = models.ForeignKey(Reps, on_delete=models.CASCADE)
+
 
 class Products(models.Model):
     product_name = models.CharField(max_length=40)
@@ -40,10 +47,12 @@ class Products(models.Model):
     def __str__(self):
         return self.product_name
 
+
 class Reps_prods(models.Model):
     rep_id = models.ForeignKey(Reps, on_delete=models.CASCADE)
     prod_id = models.ForeignKey(Products, on_delete=models.CASCADE)
     count = models.IntegerField()
+
 
 class Markets_prods(models.Model):
     market_id = models.ForeignKey(Markets, on_delete=models.CASCADE)
@@ -54,6 +63,7 @@ class Markets_prods(models.Model):
         all = Markets_prods.objects.all()
         return all
 
+
 class Clients_prods(models.Model):
     client_id = models.ForeignKey(Shopper, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Products, on_delete=models.CASCADE)
@@ -61,13 +71,17 @@ class Clients_prods(models.Model):
     pay = models.IntegerField()
     count = models.IntegerField()
 
-    def change(self,count,user_id,product_id):
+    def change(count, user_id, product_id):
         if count == 0:
-            client_product = Clients_prods.objects.get(client_id=user_id, product_id=product_id)
+            client_product = Clients_prods.objects.get(
+                client_id=user_id, product_id=product_id
+            )
             client_product.delete()
             return 1
         elif count > 0:
-            client_product = Clients_prods.objects.get(client_id=user_id, product_id=product_id)
+            client_product = Clients_prods.objects.get(
+                client_id=user_id, product_id=product_id
+            )
             client_product.count = count
             client_product.save()
             return 2
@@ -78,6 +92,7 @@ class Clients_prods(models.Model):
 class Clients_orders(models.Model):
     client_id = models.ForeignKey(Shopper, on_delete=models.CASCADE)
     order_info = models.TextField()
+
 
 class Orders_prods(models.Model):
     order_id = models.ForeignKey(Clients_orders, on_delete=models.CASCADE)
